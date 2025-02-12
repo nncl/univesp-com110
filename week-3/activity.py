f55262aad484dc2094ca71fcfb3a2aed7e66ebdd
Caue Almeida <nncl@live.com>
2025-02-11 21:04:54 -0300

    feat: Displays Items
    Resolves 6th question

--- week-3/activity.py
+++ week-3/activity.py
@@ -1,7 +1,19 @@
-invalid_names = ["hello", "world"]
-
 def get_index_by_name(name):
+    invalid_names = ["hello", "world"]
     return invalid_names.index(name)
 
 print(get_index_by_name("hello"))
 print(get_index_by_name("world"))
+
+def display_list_items():
+    list_example = ['Conceito Tuplas', 'Fazer exercício 3', 345, ['rever slide 6', 334]]
+
+    print(list_example[0])
+
+    print(list_example)
+
+    list_example[0] = 45
+
+    print(list_example)
+
+display_list_items()
