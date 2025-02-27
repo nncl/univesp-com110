594878d3675d974a45c893a5c130b33f95232ec0
Caue Almeida <nncl@live.com>
2025-02-26 21:14:14 -0300

    refactor: Creates early returns
    Instead of using nested statements.

--- week-5/class-16/triangle.py
+++ week-5/class-16/triangle.py
@@ -3,24 +3,20 @@ def drawAndDefineTriangleType():
     side_b = eval(input("Enter side B: "))
     side_c = eval(input("Enter side C: "))
 
-    bigger_side = 0
-    if side_a > bigger_side:
-        bigger_side = side_a
-    if side_b > bigger_side:
-        bigger_side = side_b
-    if side_c > bigger_side:
-        bigger_side = side_c
+    bigger_side = max(side_a, side_b, side_c)
 
-    if bigger_side < side_a + side_b + side_c - bigger_side:
-         print("Congratulations! It's a triangle.")
+    if bigger_side >= side_a + side_b + side_c - bigger_side:
+        return print("It's NOT a triangle.")
 
-         if side_a == side_b and side_b == side_c and side_a == side_c:
-             print("Equilateral")
-         elif side_a != side_b and side_b != side_c and side_a != side_c:
-             print("Scalene")
-         else:
-             print("Isosceles")
-    else:
-        print("It's NOT a triangle.")
+    print("Congratulations! It's a triangle.")
+
+    if side_a == side_b and side_b == side_c and side_a == side_c:
+        return print("Equilateral")
+
+    if side_a != side_b and side_b != side_c and side_a != side_c:
+        return print("Scalene")
+
+    return print("Isosceles")
+
 
 drawAndDefineTriangleType()
