e34d1b4b068fb20393503635270470ff8856311a
Caue Almeida <nncl@live.com>
2025-03-04 11:27:47 -0300

    refactor: Check prime numbers with `continue` instead

--- week-6/prime.py
+++ week-6/prime.py
@@ -1,12 +1,14 @@
 import unittest
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+def is_prime(number: int) -> bool:
+    i = 2
+
+    while i < number:
+        if number % i == 0:
+            break
+        i += 1
+
+    return i == number
 
 class TestPrimeFunction(unittest.TestCase):
     def test_prime_numbers(self):
