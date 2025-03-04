cc75f71d88d07bb0dd0845e2497ce6a73b6325f2
Caue Almeida <nncl@live.com>
2025-03-04 12:05:42 -0300

    feat(week-6): Creates activity file

---
+++ week-6/activity.py
@@ -0,0 +1,13 @@
+# 4 * 3 * 2 = 24
+def asks_factorial() -> int:
+    num = eval(input("Enter a number: "))
+    factorial = 1 # <-
+    x = 1
+
+    while x <= num:
+        factorial *= x # <-
+        x += 1
+
+    return factorial
+
+print(asks_factorial())
