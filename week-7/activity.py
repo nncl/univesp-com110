6af2e7a12a0294fff30701d5636c1cb32bc321f2
Caue Almeida <nncl@live.com>
2025-03-11 21:17:52 -0300

    feat(week-7): Practices printing formats

---
+++ week-7/activity.py
@@ -0,0 +1,7 @@
+print("The median is: {:.2f}".format(9.99993)) # Two decimal places
+print("The median is:", 9.99993)
+
+matrix = [ [3, 4, 5], [5, 6, 7], [7, 6, 5] ]
+for i in range(0, 3):
+    print(matrix[i])
+
