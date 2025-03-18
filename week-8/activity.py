2a2570773d3d819e475f2fb1b3f4781c8b56aaf8
Caue Almeida <nncl@live.com>
2025-03-18 17:58:55 -0300

    feat(week-8): Debugs variable declaration

---
+++ week-8/activity.py
@@ -0,0 +1,12 @@
+def numCalcula(num):
+    num = str(num)
+
+    tamanho = len(num)
+
+    return tamanho
+
+
+numero = eval(input("Entre um número: "))
+
+print("Quantidade de dígitos do numero é: ", numCalcula(numero))
+print(tamanho) # Not declared globally
